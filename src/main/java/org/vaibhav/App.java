d35d62b7a4922793b8e9b55fcea6a25c87f560a7
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-05-25 09:23:31 +0000

    modified pool configurations

--- src/main/java/org/vaibhav/App.java
+++ src/main/java/org/vaibhav/App.java
@@ -54,42 +54,46 @@ public class App {
   
   private static long getCountOnYugabyte(String ybEndpoint) throws Exception {
     // Connection conn = DriverManager.getConnection("jdbc:yugabytedb://" + ybEndpoint + ":5433/yugabyte?user=yugabyte&password=yugabyte");
-    Connection conn = ybDataSource.getConnection();
-    Statement st = conn.createStatement();
-    
-    ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + TABLE_NAME + ";");
-    rs.next();
+    try (Connection conn = ybDataSource.getConnection()){
+      Statement st = conn.createStatement();
+      
+      ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + TABLE_NAME + ";");
+      rs.next();
 
-    // st.close();
-    // conn.close();
-    return rs.getLong(1);
+      return rs.getLong(1);
+    } catch (Exception e) {
+      throw e;
+    }
   }
 
   private static void verifyCountOnMySql(String mysqlEndpoint, long countInYugabyte) throws Exception {
     // Create connection
     // Connection conn = DriverManager.getConnection("jdbc:mysql://" + mysqlEndpoint + ":3306/test_api?user=mysqluser&password=mysqlpw&sslMode=required");
-    Connection conn = mysqlDataSource.getConnection();
-    Statement st = conn.createStatement();
-    
-    // Do a select count(*)
-    long countInMysql = 0;
-    long start = System.currentTimeMillis();
+    try (Connection conn = mysqlDataSource.getConnection()) {
+      Statement st = conn.createStatement();
+      
+      // Do a select count(*)
+      long countInMysql = 0;
+      long start = System.currentTimeMillis();
 
-    // Continue for a minute if count is not the same
-    while ((System.currentTimeMillis() - start) < 60000 && countInMysql != countInYugabyte) {
-      ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + TABLE_NAME + ";");
-      rs.next();
+      // Continue for a minute if count is not the same
+      while ((System.currentTimeMillis() - start) < 60000 && countInMysql != countInYugabyte) {
+        ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + TABLE_NAME + ";");
+        rs.next();
 
-      countInMysql = rs.getLong(1);
-    }
-    
-    if (countInMysql != countInYugabyte) {
-      System.out.println("Exiting the app because count is not equal");
-      System.out.println("Yugabyte: " + countInYugabyte + " MySql: " + countInMysql);
+        countInMysql = rs.getLong(1);
+      }
+      
+      if (countInMysql != countInYugabyte) {
+        System.out.println("Exiting the app because count is not equal");
+        System.out.println("Yugabyte: " + countInYugabyte + " MySql: " + countInMysql);
 
-      System.exit(-11);
-    } else {
-      System.out.println("Count in both source and sink equal");
+        System.exit(-11);
+      } else {
+        System.out.println("Count in both source and sink equal");
+      }
+    } catch (Exception e) {
+      throw e;
     }
   }
 
@@ -97,68 +101,68 @@ public class App {
     initializeYugabyteDataSource(endpoint /* yugabyte endpoint */);
     initializeMySqlDataSource(mysqlEndpoint);
 
-    Connection conn = ybDataSource.getConnection();
-    Statement st = conn.createStatement();
-    TABLE_NAME = tableName;
-    // set up the table if it doesn't exist
-    boolean res = st.execute("create table if not exists " + TABLE_NAME + " (id int primary key, " +
-      "name text default 'Vaibhav', a bigint default 12, b float default 12.34, vrchr varchar(20) default 'varchar_column'," +
-      "dp double precision default 567.89, user_id text default '1234abcde') split into 10 tablets;");
-    if (!res && firstTime) {
-      // this means that the table is created
-      System.out.println("Table created for the first time, waiting for 10s to let the " +
-        "deployment happen");
-      firstTime = false;
-      Thread.sleep(10000);
-    }
-
-    // make sure the table doesn't contain anything
-    // st.execute("delete from test_cdc_app;");
-
-    long startKey = startMarker;
-    long endKey;
-    while(true) {
-      endKey = startKey + 511; // Total batch size would be 512
-
-      long countInYb = 0;
-      // insert rows first
-      if (!insertCompleted){
-        if (true) { // Do not update i anywhere
-          addBatchesToInsertStatement(st, startKey, endKey);
-
-          int[] insertBatchCount = st.executeBatch();
-          int resInsert = 0;
-          for (int cnt : insertBatchCount) {
-            resInsert += cnt;
-          }
-
-          // int resInsert = st.executeUpdate("insert into test_cdc_app(id) values (generate_series(" + startKey + "," + endKey + "));");
-          if (resInsert != 512) {
-            throw new RuntimeException("Unable to insert more rows, trying from scratch again...");
-          }
-        }
-        System.out.println("Inserts completed...");
-        insertCompleted = true;
-        Thread.sleep(200);
-
-        countInYb = getCountOnYugabyte(endpoint);
-
-        verifyCountOnMySql(mysqlEndpoint, countInYb);
+    try (Connection conn = ybDataSource.getConnection()) {
+      Statement st = conn.createStatement();
+      TABLE_NAME = tableName;
+      // set up the table if it doesn't exist
+      boolean res = st.execute("create table if not exists " + TABLE_NAME + " (id int primary key, " +
+        "name text default 'Vaibhav', a bigint default 12, b float default 12.34, vrchr varchar(20) default 'varchar_column'," +
+        "dp double precision default 567.89, user_id text default '1234abcde') split into 10 tablets;");
+      if (!res && firstTime) {
+        // this means that the table is created
+        System.out.println("Table created for the first time, waiting for 10s to let the " +
+          "deployment happen");
+        firstTime = false;
+        Thread.sleep(10000);
       }
 
-      // Clear the batch after the inserts
-      st.clearBatch();
+      long startKey = startMarker;
+      long endKey;
+      while(true) {
+        endKey = startKey + 511; // Total batch size would be 512
 
-      ++iterations;
-      System.out.println("Iteration count: " + iterations);
-      Thread.sleep(5000);
+        long countInYb = 0;
+        // insert rows first
+        if (!insertCompleted){
+          if (true) { // Do not update i anywhere
+            addBatchesToInsertStatement(st, startKey, endKey);
 
-      // mark the flags as false so that next iteration can take place
-      insertCompleted = false;
+            int[] insertBatchCount = st.executeBatch();
+            int resInsert = 0;
+            for (int cnt : insertBatchCount) {
+              resInsert += cnt;
+            }
 
-      // update the keys to be inserted
-      startKey = endKey + 1;
-      startMarker = startKey;
+            // int resInsert = st.executeUpdate("insert into test_cdc_app(id) values (generate_series(" + startKey + "," + endKey + "));");
+            if (resInsert != 512) {
+              throw new RuntimeException("Unable to insert more rows, trying from scratch again...");
+            }
+          }
+          System.out.println("Inserts completed...");
+          insertCompleted = true;
+          Thread.sleep(200);
+
+          countInYb = getCountOnYugabyte(endpoint);
+
+          verifyCountOnMySql(mysqlEndpoint, countInYb);
+        }
+
+        // Clear the batch after the inserts
+        st.clearBatch();
+
+        ++iterations;
+        System.out.println("Iteration count: " + iterations);
+        Thread.sleep(5000);
+
+        // mark the flags as false so that next iteration can take place
+        insertCompleted = false;
+
+        // update the keys to be inserted
+        startKey = endKey + 1;
+        startMarker = startKey;
+      }
+    } catch (Exception e) {
+      throw e;
     }
   }
 
