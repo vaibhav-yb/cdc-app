60a68ffab9bb84d1ea049f69c9d68858dfd449f9
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-05-30 05:24:08 +0000

    changes to the app with cleanup

--- src/main/java/org/vaibhav/App.java
+++ src/main/java/org/vaibhav/App.java
@@ -7,42 +7,44 @@ import java.sql.Statement;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-
 // This app assumed that all the updates are going through on Yugabyte already
 public class App {
-  private static long iterations = 0;
-  private static boolean firstTime = true;
-  private static long startMarker = 1;
+  private long iterations = 0;
+  private boolean firstTime = true;
+  private long startKey = 1;
 
   private static String TABLE_NAME = "";
 
-  // These flags are there to ensure that if the ybEndpoint the app is connected to, if it goes down
-  // and the app can continue connection to the other node then it resumes insertion from the point 
-  // where it threw the error
-  private static boolean insertCompleted = false;
+  private HikariDataSource ybDataSource;
+  private HikariDataSource mysqlDataSource;
 
-  private static HikariDataSource ybDataSource;
-  private static HikariDataSource mysqlDataSource;
-
-  private static void initializeYugabyteDataSource(String ybEndpoint) throws Exception {
+  private void initializeYugabyteDataSource(String ybEndpoint) throws Exception {
     HikariConfig config = new HikariConfig();
 
     config.setJdbcUrl("jdbc:yugabytedb://" + ybEndpoint + ":5433/yugabyte?user=yugabyte&password=yugabyte");
     config.setMaximumPoolSize(2);
-    
+
+    if (!ybDataSource.isClosed()) {
+      ybDataSource.close();    
+    }
+
     ybDataSource = new HikariDataSource(config);
   }
 
-  private static void initializeMySqlDataSource(String mysqlEndpoint) throws Exception {
+  private void initializeMySqlDataSource(String mysqlEndpoint) throws Exception {
     HikariConfig config = new HikariConfig();
 
     config.setJdbcUrl("jdbc:mysql://" + mysqlEndpoint + ":3306/test_api?user=mysqluser&password=mysqlpw&sslMode=required");
     config.setMaximumPoolSize(2);
 
+    if (!mysqlDataSource.isClosed()) {
+      mysqlDataSource.close();
+    }
+
     mysqlDataSource = new HikariDataSource(config);
   }
 
-  private static void addBatchesToInsertStatement(Statement st, long startKey, long endKey) throws Exception {
+  private void addBatchesToInsertStatement(Statement st, long startKey, long endKey) throws Exception {
     long i = startKey;
     while (i <= endKey) {
       // INSERT INTO test_cdc_app VALUES (i);
@@ -52,7 +54,7 @@ public class App {
     }
   }
   
-  private static long getCountOnYugabyte(String ybEndpoint) throws Exception {
+  private long getCountOnYugabyte(String ybEndpoint) throws Exception {
     // Connection conn = DriverManager.getConnection("jdbc:yugabytedb://" + ybEndpoint + ":5433/yugabyte?user=yugabyte&password=yugabyte");
     try (Connection conn = ybDataSource.getConnection()){
       Statement st = conn.createStatement();
@@ -66,7 +68,7 @@ public class App {
     }
   }
 
-  private static void verifyCountOnMySql(String mysqlEndpoint, long countInYugabyte) throws Exception {
+  private void verifyCountOnMySql(String mysqlEndpoint, long countInYugabyte) throws Exception {
     // Create connection
     // Connection conn = DriverManager.getConnection("jdbc:mysql://" + mysqlEndpoint + ":3306/test_api?user=mysqluser&password=mysqlpw&sslMode=required");
     try (Connection conn = mysqlDataSource.getConnection()) {
@@ -97,7 +99,7 @@ public class App {
     }
   }
 
-  private static void runWorkload(String endpoint, String mysqlEndpoint, String tableName) throws Exception {
+  private void runWorkload(String endpoint, String mysqlEndpoint, String tableName) throws Exception {
     initializeYugabyteDataSource(endpoint /* yugabyte endpoint */);
     initializeMySqlDataSource(mysqlEndpoint);
 
@@ -116,36 +118,31 @@ public class App {
         Thread.sleep(10000);
       }
 
-      long startKey = startMarker;
       long endKey;
       while(true) {
         endKey = startKey + 511; // Total batch size would be 512
 
         long countInYb = 0;
         // insert rows first
-        if (!insertCompleted){
-          if (true) { // Do not update i anywhere
-            addBatchesToInsertStatement(st, startKey, endKey);
 
-            int[] insertBatchCount = st.executeBatch();
-            int resInsert = 0;
-            for (int cnt : insertBatchCount) {
-              resInsert += cnt;
-            }
+        addBatchesToInsertStatement(st, startKey, endKey);
 
-            // int resInsert = st.executeUpdate("insert into test_cdc_app(id) values (generate_series(" + startKey + "," + endKey + "));");
-            if (resInsert != 512) {
-              throw new RuntimeException("Unable to insert more rows, trying from scratch again...");
-            }
-          }
-          System.out.println("Inserts completed...");
-          insertCompleted = true;
-          Thread.sleep(200);
-
-          countInYb = getCountOnYugabyte(endpoint);
-
-          verifyCountOnMySql(mysqlEndpoint, countInYb);
+        int[] insertBatchCount = st.executeBatch();
+        int resInsert = 0;
+        for (int cnt : insertBatchCount) {
+          resInsert += cnt;
         }
+
+        // int resInsert = st.executeUpdate("insert into test_cdc_app(id) values (generate_series(" + startKey + "," + endKey + "));");
+        if (resInsert != 512) {
+          throw new RuntimeException("Unable to insert more rows, trying from scratch again...");
+        }
+        System.out.println("Inserts completed...");
+        Thread.sleep(200);
+
+        countInYb = getCountOnYugabyte(endpoint);
+
+        verifyCountOnMySql(mysqlEndpoint, countInYb);
 
         // Clear the batch after the inserts
         st.clearBatch();
@@ -154,12 +151,8 @@ public class App {
         System.out.println("Iteration count: " + iterations);
         Thread.sleep(5000);
 
-        // mark the flags as false so that next iteration can take place
-        insertCompleted = false;
-
         // update the keys to be inserted
         startKey = endKey + 1;
-        startMarker = startKey;
       }
     } catch (Exception e) {
       throw e;
@@ -167,6 +160,7 @@ public class App {
   }
 
   public static void main(String[] args) {
+    App cdcApp = new App();
     // args will contain the endpoints
     if (args.length == 0) {
       System.out.println("No endpoints specified to connect to, exiting...");
@@ -178,7 +172,8 @@ public class App {
       try {
         // We are assuming that the last index being passed is mysql's connection point
         // and the first is a table name
-        runWorkload(args[index], args[args.length - 1], args[0]);
+
+        cdcApp.runWorkload(args[index], args[args.length - 1], args[0]);
       } catch (Exception e) {
         System.out.println("Exception caught: " + e);
         System.out.println("Trying again...");
